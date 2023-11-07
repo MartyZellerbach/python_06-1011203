2d0b26fcbe6e74eac354758cc20ce3ff1495a52d
mm <mamam@ooowo.pl>
2023-11-07 09:04:05 +0100

    init commit

--- typy_danych_4.py
+++ typy_danych_4.py
@@ -14,6 +14,39 @@ zb2.add(4)
 zb2.add(5)
 print(zb2)  # {2, 3, 4, 5}
 
+zbior.add(33)
+zbior.add(18)
+zbior.add(18)
+print(zbior)  # {33, 66, 777, 11, 44, 18, 22, 55}
+print(zbior.pop())  # 33
+print(zbior)  # {66, 777, 11, 44, 18, 22, 55}
+zbior.pop()
+zbior.pop()
+zbior.pop()
+print(zbior)  # {44, 18, 22, 55}  usunal trzy ostatnie ze zbioru
+
+zbior.remove(55)  # usuwamy 55
+zbior.remove(18)  # usuwamy 18
+print(zbior)  # {44, 22}
+
+zbior2 = {667, 11, 44, 18, 52, 62, 999, 999}
+print(zbior2)  # {18, 52, 999, 11, 44, 667, 62}
+
+print(zbior | zbior2)  # suma dwóch zbiorów {999, 11, 44, 18, 52, 22, 667, 62} z uwzgledniemiem ze jak powtarzaja sie dane to pokaze tylko raz
+print(zbior & zbior2)  # {44} - część wspólna
+print(zbior - zbior2)  # różnica {22}
+print(zbior.difference(zbior2))  # różnica {22} to samo co wyżej
+
+
+
+
+
+
+
+
+
+
+
 
 
 
