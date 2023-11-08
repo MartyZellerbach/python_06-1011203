0586906b0fdc183c63a2788b5e533be9fcfafdb0
mm <mamam@ooowo.pl>
2023-11-08 10:06:01 +0100

    add discount

---
+++ pliki2.py
@@ -0,0 +1,26 @@
+import chardet
+# pip install chardet
+# oraz po prawej na dole rogu Interpreter settings..
+
+file_path = 'test.log'
+
+with open(file_path, 'rb') as file: # nie jako tekst ale binarnie otwarcie czyrtanie
+    raw_data = file.read()
+
+# dla małej próbki czyli np wystąpienie jednego polskiego znaku ś może mieć błędny odczyt tak jak poniżej Turkish
+result = chardet.detect(raw_data)  # próba odszyfrowania kodowania pliku
+print(result)  # {'encoding': 'Windows-1254', 'confidence': 0.670697820753897, 'language': 'Turkish'}
+# dodajemy ręcznie np. ź i sprawdzamy jeszcze raz
+# {'encoding': 'utf-8', 'confidence': 0.7525, 'language': ''}
+encoding = result['encoding']
+print(raw_data.decode(encoding=encoding))
+# Radek
+# Dodane
+# Dodane
+# Dodane
+# DośdaneŹ
+# łóęć
+
+
+
+
