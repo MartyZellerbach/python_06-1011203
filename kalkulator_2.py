b232b08c21a0588a2c0550fa276320c7acece759
mm <mamam@ooowo.pl>
2023-11-08 10:45:23 +0100

    add discount

--- kalkulator_2.py
+++ kalkulator_2.py
@@ -18,16 +18,32 @@ while True:
 
     if odp >= '5':
         break
-    a = float(input('Podaj pierwszą liczbę '))
-    b = float(input('Podaj drugą liczbę '))
+    try:
+        a = float(input('Podaj pierwszą liczbę '))
+        b = float(input('Podaj drugą liczbę '))
 
-    if odp == '1':
-        print(f"Wynik dodawania {a} + {b} = {a + b}")
-    elif odp == '2':
-        print(f"Wynik odejmowanie {a} - {b} = {a - b}")
-    elif odp == '3':
-        print(f"Wynik mnożenie {a} * {b} = {a * b}")
-    elif odp == '4':
-        print(f"Wynik dzielenia {a} / {b} = {a / b}")
-    else:
-        print("Nie znam takiego działania...")
+        if odp == '1':
+            print(f"Wynik dodawania {a} + {b} = {a + b}")
+        elif odp == '2':
+            print(f"Wynik odejmowanie {a} - {b} = {a - b}")
+        elif odp == '3':
+            print(f"Wynik mnożenie {a} * {b} = {a * b}")
+        elif odp == '4':
+            print(f"Wynik dzielenia {a} / {b} = {a / b}")
+        else:
+            print("Nie znam taking działania...")
+    except ZeroDivisionError:
+        print("Nie dziel przez zero!!!")
+    except ValueError:
+        print("Błąd wartości, wpisz liczbę w działaniu!")
+    except TypeError:
+        print("Błąd typu")
+    except Exception as e:
+        print("Wsytapił błąd", e)
+    else:  # wykona się tylko, gdy nie wystąpi błąd
+        print("Operacja zakończyła się pomyślnie")
+    finally:
+        print("To wykona się zawsze..")
+        print("Zakończyłem obliczenia..")
+# jaki sposób przechwytujemy wyjątki w pythoni ? try - except, ewentualnie finally
+# praca domowa - za pomocą case
