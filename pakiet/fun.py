607ac664aca86352931f91ffd8dabf51d064dd8c
mm <mamam@ooowo.pl>
2023-11-10 12:42:55 +0100

    Aktualizacja...

--- pakiet/fun.py
+++ pakiet/fun.py
@@ -5,11 +5,11 @@ def powitanie():
 def info():
     print("Jestem pakietem")
 
-def info():
-    print("Jestem funkcja info z pakietu")
 
+print(__name__)
+# gdy program uruchamiany bezposrednio
+# __name__ ma  wartosc __main__
+# gdy program jest uruchamiany z pakietu
+# __name__ ma wartosc nazwy pakietu jaki wywołał np.: pakiet.fun
 if __name__ == '__main__':
-    print("Coś")
-
-powitanie()
-info()
+    print("Coś dowolnego")
